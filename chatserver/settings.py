89ae9bb2704976ff19ec568b6ae71cc0aa299b70
Ali Agboola <aliagboola1@gmail.com>
2021-05-03 22:50:02 +0100

    static files settings

--- chatserver/settings.py
+++ chatserver/settings.py
@@ -134,6 +134,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_URL = '/media/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
     os.path.join(BASE_DIR, 'media'),

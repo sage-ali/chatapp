b3249842bbffd7fc871c967da7fc722f13c16621
Ali Agboola <aliagboola1@gmail.com>
2021-05-03 16:05:02 +0100

    configure postgresql and redis

--- chatserver/settings.py
+++ chatserver/settings.py
@@ -75,11 +75,25 @@ WSGI_APPLICATION = 'chatserver.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'chatapp_dev',
+        'USER': 'django',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432',
+
     }
 }
 
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channel_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)]
+        },
+    },
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators

56f37d1e36e14d9717dec6dc1c32e8b96f27bbd1
Ali Agboola <aliagboola1@gmail.com>
2021-05-04 13:02:10 +0100

    adjust settings to use case insensitive auth

--- chatserver/settings.py
+++ chatserver/settings.py
@@ -29,6 +29,10 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 AUTH_USER_MODEL = "account.Account"
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.AllowAllUsersModelBackend',
+    'account.backends.CaseInsensitiveModelBackend'
+)
 
 
 # Application definition

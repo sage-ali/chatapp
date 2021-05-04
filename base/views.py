ded8805635d317eaa9380e8d958ac739eb389517
Ali Agboola <aliagboola1@gmail.com>
2021-05-04 07:20:39 +0100

    change home template in view

--- base/views.py
+++ base/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def home_screen_view(request):
-    return render(request, 'base/home.html', {})
+    return render(request, 'base.html', {})

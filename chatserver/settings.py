1b92835251ace97c264a9bfcfaf00ba59d241ae5
Ali Agboola <aliagboola1@gmail.com>
2021-05-04 13:33:14 +0100

    exclude images from statics

--- chatserver/settings.py
+++ chatserver/settings.py
@@ -144,7 +144,6 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR, 'media'),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_cdn')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media_cdn')

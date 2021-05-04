c18cd2979b729137cfa4c74988842112e7ccd3d8
Ali Agboola <aliagboola1@gmail.com>
2021-05-04 09:00:25 +0100

    add new app account to settings and include route

--- chatserver/urls.py
+++ chatserver/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("base.urls")),
+    path('account/', include("account.urls")),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

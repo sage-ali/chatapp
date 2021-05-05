794454c635c5faa074c067f29edb9e01f2f86f72
Ali Agboola <aliagboola1@gmail.com>
2021-05-05 12:35:12 +0100

    add routes for login and logout

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import registerPage
+from .views import registerPage, login_view, logout_view
 
 urlpatterns = [
     path('register/', registerPage, name='register'),
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
 ]
